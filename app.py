73c9858acb4861c434b658ed8623f6f413c8e80a
Abhijeet Chatterjee <abhijeet007rocks8@gmail.com>
2021-10-27 00:22:31 +0530

    Details API Testing (#22)
    * Login Endpoint Setup
    
    * Deployment
    
    * API endpoint Details

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify, abort
 from flask_cors import CORS, cross_origin
 import Login
+import Details
 
 app = Flask(__name__)
+
 
 @app.route('/', methods=['GET'])
 @cross_origin(supports_credentials=True)
@@ -21,6 +23,18 @@ def login():
         return jsonify({'userid': UserId, 'login': True})
     else:
         abort(401)
+
+
+@app.route('/details', methods=['GET', 'POST'])
+@cross_origin(supports_credentials=True)
+def details():
+    json_data = request.get_json()
+    UserId = json_data['userid']
+    patient_data = Details.returnData(UserId)
+    if patient_data is None:
+        patient_data = {"message": "No such Id"}
+    return jsonify(patient_data)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
