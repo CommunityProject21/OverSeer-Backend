71f223c103e92f361f0f6f9a058ec3bcd58370cc
abhijeet007rocks8 <abhijeet007rocks8@gmail.com>
2021-10-27 00:01:53 +0530

    API endpoint Details

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify, abort
 from flask_cors import CORS, cross_origin
 import Login
+import Details
 
 app = Flask(__name__)
 
@@ -22,6 +23,17 @@ def login():
         return jsonify({'userid': UserId, 'login': True})
     else:
         abort(401)
+
+
+@app.route('/details', methods=['GET', 'POST'])
+@cross_origin(supports_credentials=True)
+def details():
+    json_data = request.get_json()
+    UserId = json_data['userid']
+    patient_data = Details.returnData(UserId)
+    if patient_data is None:
+        patient_data = {"message": "No such Id"}
+    return jsonify(patient_data)
 
 
 if __name__ == '__main__':
