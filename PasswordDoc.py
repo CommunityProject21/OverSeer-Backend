e9ccf8206636d2437d36f48e232c3670d42b5aed
abhijeet007rocks8 <abhijeet007rocks8@gmail.com>
2022-02-16 21:51:18 +0530

    feat: Added Doctor Login

--- PasswordDoc.py
+++ PasswordDoc.py
@@ -2,17 +2,16 @@ import pandas as pd
 import numpy as np
 from Password import RandomPassword as rp
 
-patients_file_path = './static/DataSets/Details/patients.csv'
-password_file_path = './static/DataSets/Login/password.csv'
+patients_file_path = './static/DataSets/Details/doctor.csv'
+password_file_path = './static/DataSets/Login/passwordDoc.csv'
 
 # Feature to add that it checks the ID existing in the password table and for the ones that are not add new password entries for them. 
 
 patients = pd.read_csv(patients_file_path)
 data = []
-for Id in patients['Id']:
+for Id in patients['docid']:
     password = rp.generatePassword()
     data.append([Id, password])
 
-password_data = pd.DataFrame(data, columns=['Id', 'Password'])
+password_data = pd.DataFrame(data, columns=['DocId', 'Password'])
 password_data.to_csv(password_file_path);
-
