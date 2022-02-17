d901a24e1a1e3bedbbe23013b49e8f8026f1ce4b
Abhijeet Chatterjee <abhijeet007rocks8@gmail.com>
2022-02-18 01:11:28 +0530

    Update Training.py

--- Training.py
+++ Training.py
@@ -2,8 +2,8 @@
 import numpy as np
 import pandas as pd
 from scipy.stats import mode
-import matplotlib.pyplot as plt
-import seaborn as sns
+# import matplotlib.pyplot as plt
+# import seaborn as sns
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.svm import SVC
